27e70872cf3b68e4cc7439e98a8bcc5cc7d642a5
Nash12580 <nashalivicente340@gmail.com>
2024-08-13 12:10:11 -0700

    Made changes to footer

--- TiaraFront/src/Footer.js
+++ TiaraFront/src/Footer.js
@@ -4,39 +4,44 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className = "footer">
-            <div className = "social-media">
-                <div className = "social-icons">
-                    <a href = "#"><i className = "fab fa-facebook-f"></i></a>
-                    <a href = "#"><i className = "fab fa-youtube"></i></a>
-                    <a href = "#"><i className = "fab  fa-instagram"></i></a>
+            <div className = "footer-top">
+                <div className = "social-media">
+                    <div className = "social-icons">
+                        <a href = "#"><i className = "fab fa-facebook-f"></i></a>
+                        <a href = "#"><i className = "fab fa-youtube"></i></a>
+                        <a href = "#"><i className = "fab  fa-instagram"></i></a>
+                    </div>
+                    <p>Follow Us on Social Media</p>
                 </div>
-                <p>Follow Us on Social Media</p>
+                <div className = "footer-links">
+                    <div className = "footer-col">
+                        <h4>Service</h4>
+                        <ul>
+                            <li><a href = "#">Music & Performance</a></li>
+                            <li><a href = "#">Party Essentials</a></li>
+                            <li><a href = "#">Interactive Experience</a></li>
+                        </ul>
+                    </div>
+                    <div className = "footer-col">
+                        <h4>Customer Service</h4>
+                        <ul>
+                            <li><a href = "#">Terms of Service</a></li>
+                            <li><a href = "#">Privacy and Policy</a></li>
+                        </ul>
+                    </div>
+                    <div className = "footer-col">
+                        <h4>Are You A Vendor?</h4>
+                        <ul>
+                            <li><a href = "#">Accessibility</a></li>
+                            <li><a href = "#">About Us</a></li>   
+                        </ul>
+                    </div>
+                </div>
             </div>
-            <div className = "footer-cols">
-                <div className = "footer-col">
-                    <h4>Service</h4>
-                    <ul>
-                        <li><a href = "#">Music & Performance</a></li>
-                        <li><a href = "#">Party Essentials</a></li>
-                        <li><a href = "#">Interactive Experience</a></li>
-                    </ul>
-                </div>
-                <div className = "footer-col">
-                    <h4>Customer Service</h4>
-                    <ul>
-                        <li><a href = "#">Terms of Service</a></li>
-                        <li><a href = "#">Privacy and Policy</a></li>
-                    </ul>
-                </div>
-                <div className = "footer-col">
-                    <h4>Are You A Vendor?</h4>
-                    <ul>
-                        <li><a href = "#">Accessibility</a></li>
-                        <li><a href = "#">About Us</a></li>   
-                    </ul>
-                </div>
+            
+            <div className = "footer-bottom">
+                <p>© 2024 MiTiara</p>
             </div>
-            <div className = "footer-bottom"><p>© 2024 MiTiara</p></div>
         </footer>
     );
 };
