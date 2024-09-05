a83ec1f664f0d8008d4ba5da7f7a48cc727a92df
Juan <lujujose@gmail.com>
2024-09-04 20:40:19 -0700

    finished

--- TiaraFront/src/App.js
+++ TiaraFront/src/App.js
@@ -4,6 +4,7 @@ import Login from "./pages/Login";
 import SingleServiceDetails from "./pages/singleServiceDetails";
 import ErrorPage from "./pages/ErrorPage";
 import EmailPage from "./pages/EmailPage";
+import VendorClient from "./pages/VendorClient";
 
 export default function App() {
   const router = createBrowserRouter(
@@ -13,7 +14,7 @@ export default function App() {
           <Route index element={<div><h2>Main</h2></div>} />
           <Route path='test' element={<div><h2>Test</h2></div>} />
       </Route>
-      <Route path='/login' element={<Login />} />
+      <Route path='/login' element={<VendorClient />} />
       <Route path='/details' element={<SingleServiceDetails />}/>
       <Route path='/signup'>
           <Route path='email' element={<EmailPage />} />
