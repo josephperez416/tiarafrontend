b326e762f4542d61fed14b7154100bdacea9caea
Nash12580 <nashalivicente340@gmail.com>
2024-09-09 08:23:47 -0700

    Created About You card

---
+++ TiaraFront/src/pages/AboutYou.js
@@ -0,0 +1,76 @@
+import React from "react";
+import {Box, Card, CardContent, TextField, Button, Typography, LinearProgress, Checkbox, FormControlLabel, IconButton} from '@mui/material';
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+
+const AboutYou = () =>{
+    return(
+        <Box sx={{display:'flex', justifyContent:'center', alignItems:'center', height:'100vh',
+            backgroundColor:'#f4f4f4'}}>
+                <Card sx={{width:600, padding:4, borderRadius: 7, boxShadow:10}}>
+                    <Box sx={{display:'flex', alignItems:'center', justifyContent:'space-between', marginBottom:'20px'}}>
+                    <IconButton sx={{color: "#C13E8F", transition: "box-shadow 0.3s ease, transform 0.3s ease", "&:hover": {boxShadow: "0 0 0 10px rgba(193, 62, 143, 0.2)", transform: "scale(1)" }}}>
+                        <ArrowBackIcon/>
+
+                    </IconButton>
+                        <Box sx={{width:'200px', marginRight:'200px'}}>
+                            <LinearProgress variant="determinate" value={100} sx={{height:4, borderRadius:4, backgroundColor:'rgba(147, 47, 109, 0.6)', '& .MuiLinearProgress-bar':{backgroundColor: '#932F6D'}}}/>
+                        </Box>
+                    </Box>
+
+                    <CardContent sx={{textAlign:'center', marginBottom:'20px'}}>
+                        <Typography fontSize={'24px'} fontWeight="bold" gutterBottom>About You</Typography>
+                    </CardContent>
+
+                    <Box sx={{marginBottom: 2}}>
+                        <Box sx={{display:'flex', gap:2, marginBottom:2}}>
+                            <TextField fullWidth label = {<span>Full name</span>} variant = "outlined"
+                            sx={{'& label.Mui-focused': {color: '#932F6D',}, '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {borderColor: '#932F6D'},
+                                    '&:hover fieldset': {borderColor: '#E09EC7'},
+                                    '&.Mui-focused fieldset': {borderColor: '#932F6D'}}
+                                }}/>
+    
+                            <TextField fullWidth label = {<span>Email address<span style={{color:'red'}}>*</span></span>} variant = "outlined"
+                                sx={{'& label.Mui-focused': {color: '#932F6D',}, '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {borderColor: '#932F6D'},
+                                    '&:hover fieldset': {borderColor: '#E09EC7'},
+                                    '&.Mui-focused fieldset': {borderColor: '#932F6D'}}
+                                }}/>
+                        </Box>
+
+                        <TextField fullWidth label= {<span>Phone Number<span style={{color:'red'}}>*</span></span>} variant="outlined"
+                            sx={{marginBottom:2, '& label.Mui-focused': {color: '#932F6D',}, '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {borderColor: '#932F6D'},
+                                    '&:hover fieldset': {borderColor: '#E09EC7'},
+                                    '&.Mui-focused fieldset': {borderColor: '#932F6D'}}
+                            }}
+                        />
+
+                        <TextField fullWidth label={<span>Username<span style={{color:'red'}}>*</span></span>} variant="outlined"
+                            sx={{marginBottom:2, '& label.Mui-focused': {color: '#932F6D',}, '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {borderColor: '#932F6D'},
+                                    '&:hover fieldset': {borderColor: '#E09EC7'},
+                                    '&.Mui-focused fieldset': {borderColor: '#932F6D'}}
+                            }}
+                        />
+
+                        <TextField fullWidth label={<span>Bio<span style={{color:'red'}}>*</span></span>} variant="outlined" multiline
+                            sx={{marginBottom:2, '& label.Mui-focused': {color: '#932F6D',}, '& .MuiOutlinedInput-root': {
+                                    '& fieldset': {borderColor: '#932F6D'},
+                                    '&:hover fieldset': {borderColor: '#E09EC7'},
+                                    '&.Mui-focused fieldset': {borderColor: '#932F6D'}}
+                            }}
+                        />
+
+                        <Box sx={{display:'flex', justifyContent:'flex-end', marginTop:'20px'}}>
+                            <Button variant="contained" endIcon= {<ArrowForwardIcon/>} sx={{backgroundColor:'#932F6D', borderRadius:2, '&:hover':{backgroundColor: '#591C42'}}}>
+                                Next
+                            </Button>
+                        </Box>
+                     </Box>
+                </Card>
+            </Box>
+    );
+};
+export default AboutYou;
