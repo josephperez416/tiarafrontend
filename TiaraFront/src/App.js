e5d3aee29af8df66d12a4993f80cbd807c80baf8
aramirez234 <arami202@calpoly.edu>
2024-09-12 03:58:28 -0700

    added password page to route

--- TiaraFront/src/App.js
+++ TiaraFront/src/App.js
@@ -5,6 +5,7 @@ import SingleServiceDetails from "./pages/singleServiceDetails";
 import ErrorPage from "./pages/ErrorPage";
 import EmailPage from "./pages/EmailPage";
 import FullListingsPage from "./pages/FullListingsPage";
+import CreatePassword from "./pages/CreatePassword";
 
 export default function App() {
   const router = createBrowserRouter(
@@ -17,6 +18,7 @@ export default function App() {
       <Route path='/details' element={<SingleServiceDetails />}/>
       <Route path='/signup'>
           <Route path='email' element={<EmailPage />} />
+          <Route path='password' element={<CreatePassword />} />
       </Route>
     </>
     )
